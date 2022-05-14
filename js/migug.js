a3b403699d1fac4d97e54ab2698889a6660a504f
Victor Malhotra <victorrmalhotra@gmail.com>
2022-05-13 18:14:41 -0700

    Change migug.json

--- js/migug.js
+++ js/migug.js
@@ -1,3 +1,17 @@
-$.getJSON(`../JSON/Migug.json`, function(char) {
-      
-    });
+function onLoadPage(){
+  
+  $.getJSON(`../JSON/Migug.json`, function(migug) {
+    var name = migug.Name;
+    $("<div/>", {
+        'class': "container px-4 px-lg-5 mt-5",
+        text: "",
+        id: `${name}_card`,
+    }).appendTo('#topTen');
+  });
+  $("<div/>", {
+        'class': "row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center",
+        text: "",
+        id: ``,
+    }).appendTo(`${name}_card`);
+  });
+}
