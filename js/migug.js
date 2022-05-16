76fe66e290bae10a80a07c8a47fc2c6987d14dbf
Victor <Vmalho393@west-mec.edu>
2022-05-16 10:57:28 -0700

    Seperate Migug.json to seperate JSON files and use those to finish the migug.html page and migug.js file

--- js/migug.js
+++ js/migug.js
@@ -1,13 +1,33 @@
-$.getJSON(`JSON/Migug.json`, function(migug) {
-    var name = migug.name;
-    $("<div/>", {
-        'class': "col mb-5",
-        text: "d",
-        id: `${name}`,
-    }).appendTo('#topTen');
-    $("<div/>", {
-        'class': "card h-100",
-        text: "d",
-        id: `${name}_card`,
-    }).appendTo(`${name}`);
-});
+window.onload = function() {
+    try {
+        var url_string = $(location).attr("href");
+        var url = new URL(url_string);
+        var name = url.searchParams.get("name");
+        $.getJSON(`../JSON/${name}.json`, function(migug) {
+            var name = migug.name;
+            var genre = migug.Genre;
+            var authors = migug.Authors;
+            var rating = migug.Rating;
+            var chapters = migug.Chapters;
+            var description = migug.Description;
+            var image1 = migug.image1;
+            var image2 = migug.image2;
+            var image3 = migug.image3;
+            $('#migug_name').text(`${name}`);
+            $('#migug_genre').text(`Genres: ${genre}`);
+            $('#migug_authors').text(`Authors: ${authors}`);
+            for (var i = 0; i < rating; i++) {
+                document.getElementById('migug_rating').innerHTML += `<div class="bi-star-fill"></div>&nbsp;`
+            }
+            for (var i = 1; i <= chapters; i++) {
+                document.getElementById('migug_chapters').innerHTML += `<li><a class="dropdown-item" href="#">${i}</a></li>`
+            }
+            $('#migug_description').text(`Description: ${description}`);
+            document.getElementById('migug_image_one').innerHTML += `<img src="${image1}" class="d-block w-100" alt="...">`
+            document.getElementById('migug_image_two').innerHTML += `<img src="${image2}" class="d-block w-100" alt="...">`
+            document.getElementById('migug_image_three').innerHTML += `<img src="${image3}" class="d-block w-100" alt="...">`
+        })
+    } catch (err) {
+        console.log("Issues with parsing URL Parameter's - " + err);
+    }
+}
