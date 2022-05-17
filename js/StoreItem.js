74e7fda20fafcd7bd7fed4d249622c8e9f9bc790
Victor <Vmalho393@west-mec.edu>
2022-05-16 20:18:14 -0700

    Make JSON Changes

--- js/StoreItem.js
+++ js/StoreItem.js
@@ -3,8 +3,16 @@ window.onload = function() {
         var url_string = $(location).attr("href");
         var url = new URL(url_string);
         var name = url.searchParams.get("name");
-        $.getJSON(`https://demonlord6661.github.io/Eclipse-Manga-Prime/JSON/ShopItems/${name}.json`, function(migug) {
-            
+        $.getJSON(`https://demonlord6661.github.io/Eclipse-Manga-Prime/JSON/ShopItems/${name}.json`, function(item) {
+            var name = item.Name;
+            console.log(item.RelatingMigug);
+            if (item.RelatingMigug != "") {
+                var related = item.RelatingMigug;
+                $("#item_related").text(`Related Migug: ${related}`);
+            } else {
+                $("#item_related").text(``);
+            }
+            $("#item_name").text(`${name}`);
         })
     } catch (err) {
         console.log("Issues with parsing URL Parameter's - " + err);
